09c880efbce6aa41b002b3c12e2bdac537bca972
陨落的星辰 <1240662215@qq.com>
2017-08-09 11:44:32 +0800

    完善打包

--- XLua/Assets/Editor/BundlePacker.cs
+++ XLua/Assets/Editor/BundlePacker.cs
@@ -4,34 +4,57 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using LitJson;
 public class BundlePacker : Editor
 {
   static List<string> dirsList = new List<string>();
   static List<string> filesList = new List<string>();
   static string DataPath = Application.dataPath;
-  [MenuItem("AssetsBundle/Build")]
+  [MenuItem("AssetsBundle/BuildAssets")]
   public static void BuildBundle()
   {
     HandleLuaCode();
     BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
   }
+  [MenuItem("AssetsBundle/CreateBundleFile")]
+  public static void CreateBundleFile()
+  {
+    if (File.Exists(AppConst.BUNDLE_FILE_PATH)) File.Delete(AppConst.BUNDLE_FILE_PATH);
+
+    dirsList.Clear(); filesList.Clear();
+    GetDirAllFile(AppConst.STREAMING_PATH);
+
+    FileStream fs = new FileStream(AppConst.BUNDLE_FILE_PATH, FileMode.CreateNew);
+    StreamWriter sw = new StreamWriter(fs);
+    BundleFile bundle = new BundleFile();
+    bundle.bundle_version = AppConst.BUNDLE_VERSION;
+    bundle.show_version = AppConst.SHOW_VERSION;
+    for (int i = 0; i < filesList.Count; i++) {
+        string file = filesList[i];
+        string ext = Path.GetExtension(file);
+        if (file.EndsWith(".meta") || file.Contains(".DS_Store")) continue;
+
+        string md5 = LuaTools.MD5(file);
+        string key = file.Replace(AppConst.STREAMING_PATH, string.Empty);
+        bundle.file_info.Add(key,md5);
+    }
+    sw.Write(JsonMapper.ToJson(bundle));
+    sw.Close(); fs.Close();
+  }
   static void HandleLuaCode()
   {
-    string luaDir = DataPath + "/Lua/";
-    string streamPath = Application.streamingAssetsPath;
-    string luaBundlePath = streamPath + "/Lua/";
-    if (Directory.Exists(luaBundlePath)) Directory.Delete(luaBundlePath,true);
-    if (Directory.Exists(streamPath)) Directory.Delete(streamPath,true);
-    Directory.CreateDirectory(streamPath);
+    if (Directory.Exists(AppConst.STREAMING_PATH)) Directory.Delete(AppConst.STREAMING_PATH, true);
+    Directory.CreateDirectory(AppConst.STREAMING_PATH);
     dirsList.Clear(); filesList.Clear();
-    GetDirAllFile(luaDir);
-    foreach(string file in filesList){
-      if(Path.GetExtension(file).Equals(".meta")) continue;
-      string name =  file.Replace(Application.dataPath,"");
-      string newPath =  streamPath+ name;
+    GetDirAllFile(AppConst.LOCAL_LUA_PATH);
+    foreach (string file in filesList)
+    {
+      if (Path.GetExtension(file).Equals(".meta")) continue;
+      string name = file.Replace(Application.dataPath, "");
+      string newPath = AppConst.STREAMING_PATH + name;
       string dir = Path.GetDirectoryName(newPath);
-      if(!Directory.Exists(dir)) Directory.CreateDirectory(dir);
-      File.Copy(file,newPath);
+      if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+      File.Copy(file, newPath);
     }
   }
   //处理子类文件
